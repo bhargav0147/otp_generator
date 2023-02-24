a5ee64142d23b1c934ed855807a1450352a32207
Lenovo <bhargavpatel6832@gmail.com>
2023-02-25 04:04:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:otp_generator/Screen/home.dart';
+void main()
+{
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
